d47edd7f030affcde3240e34a4f80ccf3e4c321f
Mukhammadrizo <yaxyoevmuhammadrizo@gmail.com>
2022-05-26 19:27:22 +0500

    returned values are assigned

--- task01-arrays.go
+++ task01-arrays.go
@@ -6,5 +6,6 @@ func average(input [15]float32) (result float32) {
 	for _, v := range input {
 		sum += v
 	}
-	return sum / float32(len(input))
+	result = sum / float32(len(input))
+	return result
 }
--- task02-slice.go
+++ task02-slice.go
@@ -1,9 +1,9 @@
-package m
+package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
 	for i := len(input)-1; i >= 0; i-- {
 		result = append(result, input[i])
 	}
-	return
+	return result
 }
